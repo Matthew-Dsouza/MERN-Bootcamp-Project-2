ddb3c2987437412ea9fc9888f403e1ed823cfaaf
Matthew-Dsouza <matthew.dsouza.me@gmail.com>
2021-06-22 18:14:31 +0530

    fix modal close

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
-const saveChanges () => {
+// Parent Element to store cards 
+const taskContainer = document.querySelector(".task-container");
+console.log(taskContainer);
+
+const newCard = ({
+    id,
+    imageUrl,
+    taskTitle,
+    taskType,
+    taskDescription,
+}) => `<div class="col-md-6 col-lg-4 id=${id}">
+<div class="card">
+    <div class="
+                card-header
+                d-flex
+                justify-content-end
+                gap-2
+            ">
+        <button type="button" class="btn btn-outline-success">
+            <i class="fas fa-pencil-alt"></i>
+        </button>
+        <button type="button" class="btn btn-outline-danger">
+            <i class="fas fa-trash"></i>
+        </button>
+    </div>
+    <img src=${imageUrl}
+        class="card-img-top" alt="..." />
+    <div class="card-body">
+        <h5 class="card-title">
+            ${taskTitle}
+        </h5>
+        <p class="card-text">
+            ${taskDescription}
+        </p>
+        <span class="badge bg-primary p-2">${taskType}</span>
+    </div>
+    <div class="card-footer text-muted">
+        <button type="button" class="btn btn-outline-primary float-end">
+            Open Task
+        </button>
+    </div>
+</div>
+</div>`;
+
+const saveChanges = () => {
     const taskData = {
-        id: `${Date.now()}`,
+        id: `${Date.now()}`, //unique number for card id
         imageUrl: document.getElementById("imageUrl").value,
-        taskTitle: document.getElementById("tasktitle").value,
-        taskType: document.getElementById("tasktype").value,
-        taskDescription: document.getElementById("taskdescription").value,
+        taskTitle: document.getElementById("taskTitle").value,
+        taskType: document.getElementById("taskType").value,
+        taskDescription: document.getElementById("taskDescription").value,
     };
 
-    console.log(taskData);
+    const createNewCard = newCard(taskData);
+
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
 };
