f807c353133d544e55873af7f7fb1278a1f8bd50
Matthew-Dsouza <matthew.dsouza.me@gmail.com>
2021-06-22 16:50:44 +0530

    added search bar & card

---
+++ script.js
@@ -0,0 +1,11 @@
+const saveChanges () => {
+    const taskData = {
+        id: `${Date.now()}`,
+        imageUrl: document.getElementById("imageUrl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value,
+    };
+
+    console.log(taskData);
+};
