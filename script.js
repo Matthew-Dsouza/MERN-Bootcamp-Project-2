25d89b65762ebb92268ae700038fce82f58978da
Matthew-Dsouza <matthew.dsouza.me@gmail.com>
2021-06-26 19:01:46 +0530

    added save changes functionality

--- script.js
+++ script.js
@@ -1,118 +1,63 @@
-// Parent element to store cards
-const taskContainer = document.querySelector(".task__container");
+// stores the card container element inside a variable
+const taskContainer = document.querySelector(".task-container");
 
-// Global Store
-let globalStore = [];
-
+// creates a function which destructures the object and provides it's values inside the template literal code
 const newCard = ({
-  id,
-  imageUrl,
-  taskTitle,
-  taskDescription,
-  taskType,
-}) => `<div class="col-md-6 col-lg-4" id=${id}>
+    id,
+    imageUrl,
+    taskTitle,
+    taskType,
+    taskDescription,
+}) => `<div class="col-md-6 col-lg-4 mb-3" id=${id}>
 <div class="card">
-  <div class="card-header d-flex justify-content-end gap-2">
-    <button type="button" class="btn btn-outline-success">
-      <i class="fas fa-pencil-alt"></i>
-    </button>
-    <button type="button" id=${id} class="btn btn-outline-danger" onclick="deleteCard.apply(this, arguments)">
-      <i class="fas fa-trash-alt" id=${id} onclick="deleteCard.apply(this, arguments)"></i>
-    </button>
-  </div>
-  <img
-    src=${imageUrl}
-    class="card-img-top"
-    alt="..."
-  />
-  <div class="card-body">
-    <h5 class="card-title">${taskTitle}</h5>
-    <p class="card-text">
-      ${taskDescription}
-    </p>
-    <span class="badge bg-primary">${taskType}</span>
-  </div>
-  <div class="card-footer text-muted">
-    <button type="button" class="btn btn-outline-primary float-end">
-      Open Task
-    </button>
-  </div>
+    <div
+        class="card-header d-flex justify-content-end gap-2"
+    >
+        <button
+            type="button"
+            class="btn btn-outline-success"
+        >
+            <i class="fas fa-edit"></i>
+        </button>
+        <button
+            type="button"
+            class="btn btn-outline-danger"
+        >
+            <i class="fas fa-trash"></i>
+        </button>
+    </div>
+    <div class="card-body">
+        <img
+        src=${imageUrl}
+            class="card-img-top rounded mb-2"
+            alt="card image"
+        />
+        <h5 class="card-title">${taskTitle}</h5>
+        <p class="card-text">
+            ${taskDescription}
+        </p>
+        <span class="badge bg-primary">${taskType}</span>
+    </div>
+    <div class="card-footer text-muted">
+        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+    </div>
 </div>
 </div>`;
 
-const loadInitialTaskCards = () => {
-  // access localstorage
-  const getInitialData = localStorage.getItem("tasky"); // null
-  if (!getInitialData) return;
-
-  // convert stringified-object to object
-  const { cards } = JSON.parse(getInitialData);
-
-  // map around the array to generate HTML card and inject it to DOM
-  cards.map((cardObject) => {
-    const createNewCard = newCard(cardObject);
-    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
-    globalStore.push(cardObject);
-  });
-};
-
-const updateLocalStorage = () =>
-  localStorage.setItem("tasky", JSON.stringify({ cards: globalStore }));
-
 const saveChanges = () => {
-  const taskData = {
-    id: `${Date.now()}`, // unique number for card id
-    imageUrl: document.getElementById("imageurl").value,
-    taskTitle: document.getElementById("tasktitle").value,
-    taskType: document.getElementById("tasktype").value,
-    taskDescription: document.getElementById("taskdescription").value,
-  };
+    
+    // created an object to store the task-card data
+    const taskData = {
+        id: `${Date.now()}`,
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskType: document.getElementById("tasktype").value,
+        taskDescription: document.getElementById("taskdescription").value,
+    };
 
-  // HTML code
-  const createNewCard = newCard(taskData);
+    // created a variable to call the function & pass the object as argument
+    const createNewCard = newCard(taskData);
 
-  taskContainer.insertAdjacentHTML("beforeend", createNewCard);
-  globalStore.push(taskData);
-
-  // add to localstorage
-  updateLocalStorage();
+    // called the task-container to insert the html used to create cards and specify the position & function to be used to create the cards
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard);
 };
-
-const deleteCard = (event) => {
-  // id
-  event = window.event;
-  const targetID = event.target.id;
-  const tagname = event.target.tagName; // BUTTON
-
-  // search the globalStore, remove the object which matches with the id
-  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
-
-  updateLocalStorage();
-
-  // access DOM to remove them
-
-  if (tagname === "BUTTON") {
-    // task__container
-    return taskContainer.removeChild(
-      event.target.parentNode.parentNode.parentNode // col-lg-4
-    );
-  }
-
-  // task__container
-  return taskContainer.removeChild(
-    event.target.parentNode.parentNode.parentNode.parentNode // col-lg-4
-  );
-};
-
-// Features
-
-// Edit -> Required
-
-// HINT
-
-// Complicated yet easy -> 
-
-// 1. contenteditable
-// 2. setAttributeNode()
-
-// Open (Optional)
